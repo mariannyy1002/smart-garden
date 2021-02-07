53027c36a5c565d30e5f36b172335374c2720f2e
Alan Ortega <alanalejandrooe@hotmail.com>
2021-02-07 12:35:42 -0700

    rutas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-
+import Navegacion from './Componentes/Compartido/Navegacion';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Plantas from "./Componentes/Plantas";
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Switch>
+        <>
+        <Route path='/Plantas' exact children={<Plantas/>}/>
+        </>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Componentes/Plantas.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Plantas extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Plantas</h1>
+            </div>
+        )
+    }
+}
