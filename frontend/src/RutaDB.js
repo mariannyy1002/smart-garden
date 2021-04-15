d307543c82d4ba8254db633ddf6b64883f4a96d2
Alan Ortega <alanalejandrooe@hotmail.com>
2021-04-15 15:28:52 -0700

    Update RutaDB.js

--- frontend/src/RutaDB.js
+++ frontend/src/RutaDB.js
@@ -2,8 +2,9 @@
 //var direccion = '192.168.1.70';
 
 var direccion = 'smart-garden-db.herokuapp.com';
-var puerto = '';
+var puerto = '5000';
 
 export function rutaBase() {
-    return "http://" + direccion + ":" + puerto;
+    //return "http://" + direccion + ":" + puerto;
+    return "http://" + direccion;
 }
